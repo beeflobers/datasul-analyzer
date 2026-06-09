0a6929cf15390e9a8afc866937c98ef5d7c44eaa
beeflobers <mirelaaraujosilva@gmail.com>
2026-06-09 12:58:05 -0300

    09.06.26

--- src/hooks/useAnalyzer.js
+++ src/hooks/useAnalyzer.js
@@ -24,7 +24,7 @@ const extrairPDF = async(arrayBuffer) => {
   const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
   let text = '';
 
-  for (let i = 1; 1 <= pdf.numPages; i++) {
+  for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     const content = await page.getTextContent();
     const pageText = content.items.map(item => item.str).join(' ');
